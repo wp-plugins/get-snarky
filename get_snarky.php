a8cca06554f561ade85aa545de1336e49f9f9f17
norcross <norcross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-06 20:58:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/get-snarky/trunk@238095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ get_snarky.php
@@ -0,0 +1,177 @@
+<?php
+/*
+Plugin Name: Get Snarky
+Plugin URI: http://andrewnorcross.com/plugins/get-snarky
+Description: 'Inspired' by the original Hello Dolly plugin, this just spits out random stuff I find funny. Some are a bit NSFW
+Version: 1.2
+Author: Andrew Norcross
+Author URI: http://andrewnorcross.com
+*/
+
+/*  Copyright 2010 Andrew Norcross
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License (GPL v2) only.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function hello_snarky_get_quote() {
+	/** These are my quotes */
+	$quotes = "Violence only works if you use it
+Don't hit kids. No seriously, they have guns now
+It's all about hugs & understanding. And spearguns. Don't forget the spearguns.
+An orchestra conductor is basically a guy who takes a shitload of credit for pushing the ''play'' button.
+They're not all gems, folks.
+Work is starting a ''Fun Committee.'' I'd consider joining if we could replace ''Fun'' with ''Stab.''
+I will rule with the Iron Fist of Apathy. Or not. Whatever.
+The only thing between you and a throatpunch is this coffee. If I were you, I'd brew another pot.
+America: We will kill you in your sleep on Christmas!
+Ever hear a rubber chicken scream? I have and it's not pretty.
+In the Russian Roulette game of life, today was a ''Click. Phew''.
+Bukowski weekend starts now. Prepare the Liver Punch!
+Drunk crosswording. I'm not sure what level of pathetic that qualifies me for.
+People who stress out about eating healthy and organic are tedious. Ego-centrism doesn't need a good cause. Just do it.
+I just fed a coyote a corndog. That sums up my Saturday pretty well.
+Fun Fact*: Unicorn farts smell like gumdrops. *Facts may not be true.
+A dictionary? Okay. A thesaurus? Maybe. A usage book? Get the fuck out of my face.
+Gianormous Spiders: Nature's reminder that you are, in fact, a little girl
+The wife just made me a Lumpy. Seriously, ''Smoothie'' does not apply in this situation.
+Peace stagnates. Conflict mobilizes. Remember that.
+Kids, you don't have to listen in class anymore. The internet exists.
+Drugs - FUCK They Make Us Dance Awesome
+I have caffeinated myself to a Zen-like state of enlightenment.
+When someone tells you they are expecting, ''Guess you failed the Save vs Conception roll, huh?'' is not an appropriate response.
+''Wanna listen to Jethro Tull?'' ''No. I'm just gonna go play Dungeons and Dragons in a damp field.''
+The Mrs' driving may or may not make me involuntarily release urine into my trousers.
+I still can't get over the fact that we elected our first articulate president.
+It's always the retard with the Flame Thrower. ALWAYS.
+Muppets Can Have Bad Days, Too
+No matter where you live in L.A., you're only about a mile from someplace terrible.
+The way today is going I think even receiving a blow job would piss me off.
+Four words you never want to hear in a bathroom: ''Hey, is this leprosy?''
+I'm all puppy kisses and unicorn farts. Who wouldn't want to be around me?
+If I was an Arab, I'd hate Jews, but I'm a Jew, so I just kind of hate Jews.
+Clowns are just Satan's idea of what's funny
+You know it's way over the line when a joke makes me cringe. No, it wasn't the last one, it's the ones I haven't shared.
+You can stop writing songs about the rain. We get it.
+Taking your shoes & socks off probably breaches some heathen form of workplace etiquette
+Even at the age of 12, I knew St. Elmo's Fire was bullshit. Wonderful bullshit.
+9/11 kinda put an end to that ''bongo drum in the park'' guy's relevance. So, not all bad.
+It's close but, I think I'd rather be Magic with HIV than a healthy Bird.
+My job has taught me never to say, ''It can't get any worse.'' Old Milwaukee ads taught me never to say, ''It don't get no better than this.''
+You would think security would be tighter around the Master Plan
+If life hands you lemons, ask it if it knows where my 1987 mix tape is because it should know shit like that.
+It's a good day when you can work ''succulent face meat'' into a conversation.
+Most underappreciated hero of our time? Nerfman
+How do I know if something is inappropriate? If it makes makes me laugh for 5 minutes or longer
+Dreamt I was writing a song with Alan Jackson, Toby Keith & some other famous country star. Yeah, I have no idea either.
+Interesting how a little bit of fire makes people skittish.
+Note to managers: just because my job requires me to use the Internet does not mean I have some magical power over space & time.
+After seeing the headline ''Baby suffers burns after hot bath'' all I could think was ''Didn't they learn from Rain Man''?
+Tonight's dinner is a blast from the past: pure, unadulterated hate.
+Remember, abstinence is only 99.99% effective
+Dude, you do not have a tail. Just admit you shit yourself and move on.
+You haven't lived until you've bet on Hungry Hungry Hippos for shots.
+Oompa loompas don't sing in heaven. They tidy up the clouds.
+You can stop clapping now if you want. Really. You'll need your energy for cheering me later.
+Don't... Don't put the noodles and the dumplings together in the boat. They'll fight! The noodles are bullies. Poor dumplings.
+I always get people gifts that I would want. Therefore, this year, you're getting a gun.
+I haven't put on weight. Your eyes are fat.
+If you shit your pants you get to go home early.
+If someone tells you they fail at life, this means they are undead and should be destroyed immediately.
+Is there any kind of buttsex other than SURPRISE buttsex?
+Sweetie, I would never call you a dumbass unless I meant it.
+There's things I have to do in my life, but catching an STD in front of a live audience is not one of them.
+Why are people always stealing my ideas? Like penis in vagina sex. MY IDEA.
+I need romance! Like girlie mags! A fifth of rum! ROMANCE!
+If you gave me a car made of diamonds and blowjobs all day I still wouldn't drink Coors Light.
+You cannot intimidate a browser with profanity, threats or physical violence. They just laugh at you in binary.
+You only think Kim Jong Il is in charge. He is the perfect puppet of the Platypus & Panda Proletariat.
+The correct response to a project status is not ''I set it on fire & let it burn to the ground, whereupon I laughed & danced upon its ashes.''
+Know what I love about you? No, love's the wrong word. Know what I HATE about you?
+Unintelligent design. Where I pay some drunk guy to design the universe. It'll explain flightless birds.
+Why don't you just cut off my thumbs and call it a complete evening
+Down south we have a saying: you can't turn a lobster into a shrimp.
+Since 9/11, the NYC skyline looks like a smiling kid who's missing his 2 front teeth.
+Telling your live-in girlfriend ''I can't hang out tonight, I have a date'' tends to end badly.
+The fact that American Standard is the name of a urinal says a lot about our culture.
+While at a funeral, if offered sacramental wine do not pull out a flask & say, ''It's cool, I got it.''
+Don't leave the duck there. It's totally irresponsible. Put it on the swing, it'll have much more fun.
+I demand compensation in cola bottles. Lots of fizzy cola bottles. In one lump sum.
+Of course the zombie loved me. She gave me her heart. Mmmmm-hmmm. And her hand in marriage.
+Keep looking at the sun like that, your eyes will dry up like raisins and fall out of your skull.
+Singing & dancing, screaming & writhing...really, it's all a matter of perception.
+I have a Weasel on a Stick because being beaten with a stick is embarrassing enough. Weasel bites are a shame that never go away
+Look at the size of your bath. I can pee in it and you'd never notice.
+There is a Buddy Christ on my desk. When people say stupid things I put water on its face and tell them, ''You make Jesus cry.''
+Bad Interview Answers: There was a lot of yelling so not sure if I quit or was fired. The fire wasn't MY fault, though.
+I've got two things to say to you: fuck and off. Capish?
+When asked to describe the workplace environment, do not respond, ''Working here hurts my soul.''
+Placing a rubber duck in the water cooler doesn't make the office ''closer to nature.''
+Coworker: Are you living the dream? Me: If by ''dream'' you mean ''wake up screaming while praying for death,'' then yeah.
+Who has two thumbs and just spent $20 downloading tracks by 80's Japanese metal band Loudness? Yeah, probably not YOU.
+Buy this truck and PUNCH MOTHER NATURE IN THE FACE.
+Is it okay that before almost every decision I make I think, ''Okay. What wouldn't dad do?''
+If you see a maximum occupancy sign in a funeral home, do not ask if that includes the corpse.
+I'd kick Martha Stewart's ass at Scrabble
+For the past two days I've had a headache that could kill a moderately hardy guanaco.
+Really want to surprise someone? Throw a 40th birthday party on their 29th birthday. They'll never expect that.
+I felt like snuffing the christmas cheer from their vacant little eyes.
+I think I'll ask the dentist to install tusks in my face so I can fully embrace my orcish heritage.
+Yeah, crazy girls are the best in bed. They're also the most likely to bite off your weiner.
+You know, teddy bears and robots are mortal enemies. Really, it's been a few years now.
+I'm really good at lying in bed naked. World class.
+It's not so much the fact there are voices in my head, it's more that they're SCREAMING.
+Dating Tip: When asked for your People/Celebrities You're Allowed to Sleep With List, don't fill it with people you actually know.
+We have determined to hash out our differences over Percocet and whores.
+You can prevent crying while peeling onions by chewing gum. You can prevent crying in other situations by not being such a little bitch.
+Emo: It's Like Goth, But For Pussies";
+
+	// Here we split it into lines
+	$quotes = explode("\n", $quotes);
+
+	// And then randomly choose a line
+	return wptexturize( $quotes[ mt_rand(0, count($quotes) - 1) ] );
+}
+
+// This just echoes the chosen line, we'll position it later
+function hello_snarky() {
+	$chosen = hello_snarky_get_quote();
+	echo "<p id='snarky'>$chosen</p>";
+}
+
+// Now we set that function up to execute when the admin_footer action is called
+add_action('admin_footer', 'hello_snarky');
+
+// We need some CSS to position the paragraph
+function snarky_css() {
+	// This makes sure that the posinioning is also good for right-to-left languages
+	$x = ( 'rtl' == get_bloginfo( 'text_direction' ) ) ? 'left' : 'right';
+
+	echo "
+	<style type='text/css'>
+	#snarky {
+		position: absolute;
+		top: 4.2em;
+		margin: 0;
+		padding: 0;
+		$x: 215px;
+		font-size: 11px;
+		font-weight: bold;
+	}
+	</style>
+	";
+}
+
+add_action('admin_head', 'snarky_css');
+
+?>
